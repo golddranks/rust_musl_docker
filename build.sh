053667a6f2bd87e3ec4f0c023354074fc56b597d
Kontio Pyry <pyry.kontio@drasa.eu>
2017-07-26 20:34:04 +0900

    Now supports building any specified nightly or stable.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -xe
+build_docker () {
+    sed "s/TOOLCHAIN/$1/g" Dockerfile.template > Dockerfile
+    docker build -t golddranks/rust_musl_docker:$1 .
+    docker push golddranks/rust_musl_docker:$1
+};
+build_docker "nightly-2017-07-20"
+build_docker "nightly-2017-07-19"
