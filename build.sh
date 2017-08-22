f99279e02cdf7cda34e3300de877b1683a63f669
Pyry Kontio <pyry.kontio@drasa.eu>
2017-08-22 15:52:47 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 build_docker () {
     sed "s/TOOLCHAIN/$1/g" Dockerfile.template > Dockerfile
-    docker build -t golddranks/rust_musl_docker:$1 .
-    docker push golddranks/rust_musl_docker:$1
+    docker build -t $1:$2 .
+    docker push $1:$2
 };
-build_docker "nightly-2017-08-21"
+build_docker "golddranks/rust_musl_docker" "nightly-2017-08-21"
