1381cf898a97582aeb1ecc93f925f9ab580c8c08
Kontio Pyry <pyry.kontio@drasa.eu>
2017-08-22 16:52:00 +0900

    Fixed the build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 build_docker () {
-    sed "s/TOOLCHAIN/$1/g" Dockerfile.template > Dockerfile
-    docker build -t $1:$2 .
-    docker push $1:$2
+    sed "s/TOOLCHAIN/$2/g" Dockerfile.template > Dockerfile
+    docker build -t "$1":"$2" .
+    docker push "$1":"$2"
 };
 build_docker "golddranks/rust_musl_docker" "nightly-2017-08-21"
