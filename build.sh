f9ad2caadd64e048d19ce799a96c8238bc96f123
Kontio Pyry <kontio@r.recruit.co.jp>
2018-02-16 23:12:41 +0900

    Made the tag names to include the openssl version.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ build_docker () {
     cat Dockerfile.template |
     sed "s/RUST_TOOLCHAIN/$2/g" |\
     sed "s/OPENSSL_VER/$3/g" > Dockerfile
-    docker build -t "$1":"$2" .
-    docker push "$1":"$2"
+    docker build -t "$1":"$2_$3" .
+    docker push "$1":"$2_$3"
 };
-build_docker "golddranks/rust_musl_docker" "nightly-2018-02-15" "openssl-1.0.2n"
+build_docker "golddranks/rust_musl_docker" "nightly-2018-02-15" "openssl-1.1.0g"
