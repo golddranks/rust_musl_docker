7e48e078bf772a520b8ee1e94a9b2bf9a8860ac2
Kontio Pyry <kontio@r.recruit.co.jp>
2018-04-04 11:22:49 +0900

    Updated to Rust 1.27 nightlies + Updated OpenSSL and Postgres

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ build_docker () {
     docker build -t "$1":"$2_$3" .
     docker push "$1":"$2_$3"
 };
-build_docker "golddranks/rust_musl_docker" "nightly-2018-02-27" "openssl-1.1.0g"
+build_docker "golddranks/rust_musl_docker" "nightly-2018-04-04" "openssl-1.1.0h"
