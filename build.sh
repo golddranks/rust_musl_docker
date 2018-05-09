14652fa2ae10efe3c2f8dc5ed42314ccdee56ee3
Kontio Pyry <kontio@r.recruit.co.jp>
2018-05-09 14:50:10 +0900

    Updated nightly

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ build_docker () {
     docker build -t "$1":"$2_$3" .
     docker push "$1":"$2_$3"
 };
-build_docker "golddranks/rust_musl_docker" "nightly-2018-04-27" "openssl-1.1.0h"
+build_docker "golddranks/rust_musl_docker" "nightly-2018-05-09" "openssl-1.1.0h"
