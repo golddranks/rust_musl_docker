7ab748de4d81a1a759174ef76b30b707549c7c05
Kontio Pyry <kontio@r.recruit.co.jp>
2018-07-27 12:54:10 +0900

    Added a branch with python3

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ build_docker () {
     cat Dockerfile.template |
     sed "s/RUST_TOOLCHAIN/$2/g" |\
     sed "s/OPENSSL_VER/$3/g" > Dockerfile
-    docker build -t "$1":"$2_$3" .
-    docker push "$1":"$2_$3"
+    docker build -t "$1":"$2_$3_py3" .
+    docker push "$1":"$2_$3_py3"
 };
 build_docker "golddranks/rust_musl_docker" "nightly-2018-07-24" "openssl-1.1.0h"
