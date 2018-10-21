960e1cbebeed5edaaaa2ed9772b48f896e7e8d6e
Pyry Kontio <pyry.kontio@drasa.eu>
2018-10-21 10:36:13 +0900

    Fixing 4

---
+++ build_manual.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -xe
+
+
+# stable with an non-changing tag
+./build.sh "golddranks/rust_musl_docker" "1.29.2" "1.1.1" "11.0" "stable-1.29.2"
--- build_releases.sh
+++ build_releases.sh
@@ -2,10 +2,10 @@
 
 
 # newest beta
-./build.sh "golddranks/rust_musl_docker" "beta" "1.1.1" "11.0" "current-beta"
+./build.sh "golddranks/rust_musl_docker" "beta" "1.1.1" "11.0" "beta-latest"
 
 # newest stable
-./build.sh "golddranks/rust_musl_docker" "stable" "1.1.1" "11.0" "current-stable"
+./build.sh "golddranks/rust_musl_docker" "stable" "1.1.1" "11.0" "stable-latest"
 
 # stable with an non-changing tag
 ./build.sh "golddranks/rust_musl_docker" "1.29.2" "1.1.1" "11.0" "stable-1.29.2"
